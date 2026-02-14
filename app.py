d1a8d0308b3a59dc90881d5ea67bdea85ad59d6c
MMohammadinia <Mohammadiniyaaa@gmail.com>
2026-02-13 23:46:52 -0500

    Fix flake8 formatting correctly

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
 from flask import Flask
+
+
 app = Flask(__name__)
 
+
 @app.route("/")
-def home():
-    return "SERVICERUNNING"
+def hello():
+    return "Hello World"
+
 
 if __name__ == "__main__":
-    print("SERVICERUNNING")
-    app.run(host="0.0.0.0", port=8000)
+    app.run(host="0.0.0.0", port=8080)
+
